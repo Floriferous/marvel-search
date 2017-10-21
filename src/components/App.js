e9e4ea913103aad3a6ad6c57a60518c0f9e77266
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-21 16:51:58 +0200

    refactor App to a stateless component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 
 import configureStore from '../store';
@@ -6,14 +6,10 @@ import HomePage from './HomePage';
 
 const store = configureStore();
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <HomePage />
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <HomePage />
+  </Provider>
+);
 
 export default App;
