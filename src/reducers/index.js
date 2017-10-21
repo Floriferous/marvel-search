cf7868cb699876cc8891ad179b8339fe1d743cdd
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-21 19:47:37 +0200

    combine existing reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 
 import search from './search';
+import pagination from './pagination';
+import searchResults from './searchResults';
 
 const rootReducer = combineReducers({
   search,
+  pagination,
+  searchResults,
 });
 
 export default rootReducer;
