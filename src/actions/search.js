9a83ce37c2c41abda21dff1dd08709a1ce6948cc
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-21 19:42:43 +0200

    add search action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,2 +1,39 @@
-export const changeSearch = search => ({ type: 'CHANGE_SEARCH', search });
+import * as api from '../api';
+
+const createSearchKey = (search, pagination) => search + pagination;
+
+const fetchCharacters = (dispatch, getState) => {
+  const { search, pagination, searchResults } = getState();
+  const searchKey = createSearchKey(search, pagination);
+
+  if (searchResults[searchKey]) {
+    return Promise.resolve();
+  }
+  return api.fetchCharacters(search).then((characters) => {
+    dispatch({
+      type: 'ADD_SEARCH_RESULTS',
+      search: searchKey,
+      characters,
+    });
+  });
+};
+
+export const changeSearch = search => (dispatch, getState) => {
+  dispatch({
+    type: 'CHANGE_SEARCH',
+    search,
+  });
+
+  fetchCharacters(dispatch, getState);
+};
+
 export const resetSearch = () => ({ type: 'RESET_SEARCH' });
+
+export const changePagination = pagination => (dispatch, getState) => {
+  dispatch({
+    type: 'CHANGE_PAGINATION',
+    pagination,
+  });
+
+  fetchCharacters(dispatch, getState);
+};
