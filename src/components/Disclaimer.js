1d156d37b7275c364a43e39409f3e6bca77be4ba
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-21 21:54:09 +0200

    Add mandatory disclaimer for Marvel API use

---
+++ src/components/Disclaimer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Disclaimer = () => (
+  <div className="disclaimer">
+    <p>Data provided by Marvel. © 2014 Marvel</p>
+  </div>
+);
+
+export default Disclaimer;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Input from './Input';
 import CharacterList from './CharacterList';
+import Disclaimer from './Disclaimer';
 
 const HomePage = () => (
   <main>
     <h1>Marvel Search</h1>
     <Input />
     <CharacterList />
+    <Disclaimer />
   </main>
 );
 
