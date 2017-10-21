1aa16bfe8d32b4bb9d855bb9803d29b7bec8da35
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-21 20:26:22 +0200

    Add className to CharacterFooter for styling

--- src/components/CharacterFooter.js
+++ src/components/CharacterFooter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CharacterFooter = ({ name, isBookmarked }) => (
-  <div>
+  <div className="footer">
     <h3>{name}</h3>
   </div>
 );
