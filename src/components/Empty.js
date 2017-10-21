3bfd562674222cbcee0b221ee2d42dac5f9a5e11
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-21 20:25:00 +0200

    Add class to Empty for styling

--- src/components/Empty.js
+++ src/components/Empty.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Empty = ({ isSearching }) => (
-  <h2>
+  <h2 className="empty">
     {isSearching
       ? "Couln't find any results for this query"
       : 'Start searching or add bookmarks to find your heroes'}
