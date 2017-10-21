3799af9cf2e7eefbb5479c18ef2d65aa275991c1
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-21 17:43:27 +0200

    add Character component

---
+++ src/components/Character.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Character = ({ image, name, isBookmarked }) => (
+  <li className="character">MyComponent</li>
+);
+
+Character.propTypes = {};
+
+export default Character;
