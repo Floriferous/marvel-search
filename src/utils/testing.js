49e9ef7d34c98e7decc4edd099c362410fd51e64
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-22 15:32:40 +0200

    Add new test utils
    This function creates a new shallow component when called, but returns
    the same component during each subsequent call, so you can modify it.
    
    the reset method on the function should be called before each test.

--- src/utils/testing.js
+++ src/utils/testing.js
@@ -1,7 +1,19 @@
+import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+export const testComponent = (Component, props) => {
+  if (!testComponent.mountedLockScreen) {
+    testComponent.mountedLockScreen = Enzyme.shallow(<Component {...props} />);
+  }
+  return testComponent.mountedLockScreen;
+};
+
+testComponent.reset = () => {
+  testComponent.mountedLockScreen = undefined;
+};
 
 export const { shallow, mount, render } = Enzyme;
 export default Enzyme;
