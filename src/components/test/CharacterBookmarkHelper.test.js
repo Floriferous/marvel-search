5b3254823b39107a4c0e4fee5ad8ae6135331a7b
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-22 18:43:32 +0200

    Add tests for CharacterBookmarkHelper

---
+++ src/components/test/CharacterBookmarkHelper.test.js
@@ -0,0 +1,31 @@
+import { testComponent } from '../../utils/testing';
+import CharacterBookmarkHelper from '../CharacterBookmarkHelper';
+
+describe('CharacterBookmarkHelper', () => {
+  let props;
+  const component = () => testComponent(CharacterBookmarkHelper, props);
+  beforeEach(() => {
+    props = { isBookmarked: false };
+    testComponent.reset();
+  });
+
+  it('renders a p with class helper', () => {
+    expect(component().find('p').length).toBe(1);
+    expect(component()
+      .find('p')
+      .hasClass('helper')).toBe(true);
+  });
+
+  it('renders different test based on isBookmarked prop', () => {
+    const text1 = component()
+      .find('p')
+      .text();
+    testComponent.reset();
+    props.isBookmarked = true;
+    const text2 = component()
+      .find('p')
+      .text();
+
+    expect(text1).not.toBe(text2);
+  });
+});
