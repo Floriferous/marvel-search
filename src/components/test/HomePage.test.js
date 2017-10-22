3b0b0c46c2e0c9665ac5e9095a76e2685c1552c4
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-22 15:28:02 +0200

    Use new testing utils for HomePage tests

--- src/components/test/HomePage.test.js
+++ src/components/test/HomePage.test.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import { shallow } from '../../utils/testing';
+import { testComponent } from '../../utils/testing';
 import HomePage from '../HomePage';
 
 describe('HomePage', () => {
-  let component;
+  const component = () => testComponent(HomePage);
 
   beforeEach(() => {
-    component = () => shallow(<HomePage />);
+    testComponent.reset();
   });
 
-  // it('matches its snapshot', () => {
-  //   const tree = renderer.create(<HomePage />).toJSON();
-  //   expect(tree).toMatchSnapshot();
-  // });
+  it('matches its snapshot', () => {
+    const tree = renderer.create(<HomePage />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
   it('renders a main as its root', () => {
     expect(component().find('main').length).toBe(1);
   });
 
-  it('renders one h1, Input and CharacterList', () => {
+  it('renders one h1, Input, CharacterList, and Disclaimer', () => {
     expect(component().find('h1').length).toBe(1);
     expect(component().find('Input').length).toBe(1);
     expect(component().find('CharacterList').length).toBe(1);
+    expect(component().find('Disclaimer').length).toBe(1);
   });
 });
