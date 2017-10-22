0c47f0edecc2c8076f15456155ed23e3abb43c08
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-22 15:23:37 +0200

    Add more tests for CharacterList

--- src/components/test/CharacterList.test.js
+++ src/components/test/CharacterList.test.js
@@ -1,22 +1,85 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import { shallow } from '../../utils/testing';
-import CharacterList from '../CharacterList';
+import { testComponent } from '../../utils/testing';
+import ConnectedCharacterList, { CharacterList } from '../CharacterList';
 
 describe('CharacterList', () => {
-  let component;
+  let props;
+  const component = () => testComponent(CharacterList, props);
 
   beforeEach(() => {
-    component = props => shallow(<CharacterList {...props} />);
+    props = {};
+    testComponent.reset();
   });
 
-  // it('matches its snapshot', () => {
-  //   const tree = renderer.create(<CharacterList />).toJSON();
-  //   expect(tree).toMatchSnapshot();
-  // });
+  it('matches its snapshot', () => {
+    const tree = renderer.create(<CharacterList {...props} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders Loading if user is currently searching and no characters are provided', () => {
+    props = { isSearching: true };
+    expect(component().find('Loading').length).toBe(1);
+  });
+
+  describe('when characters is an empty array', () => {
+    beforeEach(() => {
+      props = { characters: [] };
+    });
+
+    it('renders Empty if user is currently searching', () => {
+      props.isSearching = true;
+      expect(component().find('Empty').length).toBe(1);
+    });
+
+    it('it passes isSearching to Empty', () => {
+      props.isSearching = 'test';
+      expect(component()
+        .find('Empty')
+        .props().isSearching).toBe(props.isSearching);
+    });
+  });
 
   it('renders Empty if no characters are provided', () => {
-    expect(component({ characters: null }).find('Empty').length).toBe(1);
+    props = { characters: null };
+    expect(component().find('Empty').length).toBe(1);
+  });
+
+  describe('when characters is a non empty array', () => {
+    beforeEach(() => {
+      props = { characters: [{ id: 'test1' }, { id: 'test2' }] };
+    });
+
+    it('renders a list with character-list class', () => {
+      expect(component().find('ul').length).toBe(1);
+      expect(component()
+        .find('ul')
+        .hasClass('character-list')).toBe(true);
+    });
+
+    it('renders one Character per character', () => {
+      expect(component().find('Character').length).toBe(props.characters.length);
+    });
+
+    describe('each Character', () => {
+      let characters;
+      beforeEach(() => {
+        props.toggleBookmark = 'testFunc';
+        characters = component().find('Character');
+      });
+
+      it('is passed the character as a prop', () => {
+        characters.forEach((c, i) => {
+          expect(c.props().character).toBe(props.characters[i]);
+        });
+      });
+
+      it('is passed toggleBookmark as a prop', () => {
+        characters.forEach((c) => {
+          expect(c.props().toggleBookmark).toBe(props.toggleBookmark);
+        });
+      });
+    });
   });
 });
