4005ef34df571de0d105a9e9175bce4d13d50f66
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-22 21:04:10 +0200

    Add tests for localStorage

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,6 +1,6 @@
 export const storageAvailable = () => {
   try {
-    const storage = window.localStorage;
+    const storage = localStorage;
     const x = '__storage_test__';
     storage.setItem(x, x);
     storage.removeItem(x);
@@ -13,11 +13,16 @@ export const storageAvailable = () => {
 export const saveValueAtKey = (key, value) => {
   if (storageAvailable()) {
     localStorage.setItem(key, JSON.stringify(value));
+    return true;
   }
+
+  return false;
 };
 
 export const getValueAtKey = (key) => {
   if (storageAvailable()) {
     return JSON.parse(localStorage.getItem(key));
   }
+
+  return false;
 };
