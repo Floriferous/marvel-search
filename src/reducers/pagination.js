9a2547b488bc9bb37497cbde9c21e3b847fb564c
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-23 10:29:17 +0200

    Fix pagination reducer typo

--- src/reducers/pagination.js
+++ src/reducers/pagination.js
@@ -3,9 +3,9 @@ export const initialState = 0;
 const pagination = (state = initialState, action) => {
   switch (action.type) {
     case 'DECREMENT_PAGINATION':
-      return action.pagination - 1;
+      return state - 1;
     case 'INCREMENT_PAGINATION':
-      return action.pagination + 1;
+      return state + 1;
     case 'RESET_PAGINATION':
       return initialState;
     default:
